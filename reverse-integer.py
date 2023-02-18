01aaf20a51611b7177f8a6a5721fd9ea37abca5c
garchi26 <111746592+garchi26@users.noreply.github.com>
2023-02-18 18:18:04 +0530

    Create reverse-integer.py

---
+++ reverse-integer.py
@@ -0,0 +1,29 @@
+'''
+Input: x = 123
+Output: 321
+
+Input: x = -123
+Output: -321
+
+Input: x = 120
+Output: 21
+'''
+
+class Solution:
+    def reverse(self, a: int) -> int:
+        count=0
+        b=0
+        if (a < 0):
+            a= -a
+            count =1
+        while(a != 0 ):
+            n= a % 10;
+            b= (b*10)+n;
+            a= int((a-n)/10);
+        if (count == 1):
+            b= -b
+        powr= pow(2,31) 
+        if ((b >= -powr) and (b <= powr)):
+            return b
+        else:
+            return 0
